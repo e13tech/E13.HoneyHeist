fea3c18341ed516020c3f4979b0063a9cf9e40db
JJ Bussert <jj.bussert@gmail.com>
2024-12-13 14:19:41 -0600

    working swa orchestration

--- src/E13.HoneyHeist.Aspire.AppHost/Program.cs
+++ src/E13.HoneyHeist.Aspire.AppHost/Program.cs
@@ -20,7 +20,27 @@ var api = builder
     .AddAzureFunctionsProject<Projects.E13_HoneyHeist_Api>("api")
     .WithHostStorage(storage)
     .WithReference(blobs)
+    .WithExternalHttpEndpoints()
+    ;
+
+var web = builder.AddNpmApp("web", "../E13.HoneyHeist.Web")
+    .WithEnvironment("BROWSER", "none") // Disable opening browser on npm start
+    .WithHttpEndpoint(env: "PORT")
     .WithExternalHttpEndpoints();
+
+// Retrieve the HTTP endpoint for the 'web' service
+var webEndpoint = web.GetEndpoint("http");
+
+// Retrieve the HTTP endpoint for the 'api' service
+var apiEndpoint = api.GetEndpoint("http");
+
+var swa = builder.AddSwaEmulator("swa")
+    .WithArgs(
+        "--app-devserver-url", webEndpoint,
+        "--api-devserver-url", apiEndpoint)
+    .WithReference(api)
+    .WithReference(web);
+
 
 //builder.AddProject<Projects.ExampleProject>()
 //       .WithReference(api)
